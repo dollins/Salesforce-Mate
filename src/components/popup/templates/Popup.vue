bfda7cc1804bb768b18d740f0972a9f89c5372b1
Vladislav Rakita <rakita.vladislav@gmail.com>
2023-04-18 20:47:03 +0200

    Template fix.

--- src/components/popup/templates/Popup.vue
+++ src/components/popup/templates/Popup.vue
@@ -17,35 +17,14 @@ export default {
     data() {
         return {
             active: true,
-            list: "example.com",
-            icons: {
-                active: 'images/icon-48x48.png',
-                inactive: 'images/icon-48x48-off.png'
-            }
+            list: "example.com"
         }
     },
     created() {
-        chrome.storage.sync.get(['toggleSitesActive', 'toggleSitesList'], (result) => {
-            this.active = result.toggleSitesActive;
-            this.list = result.toggleSitesList;
-        });
+
     },
     methods: {
-        setActive(active) {
-            this.active = active;
-            chrome.storage.sync.set({
-                toggleSitesActive: active
-            }, () => {});
 
-            chrome.browserAction.setIcon({
-                path: this.icons[active ? 'active' : 'inactive']
-            });
-        },
-        saveList() {
-            chrome.storage.sync.set({
-                toggleSitesList: this.list
-            }, () => {});
-        }
     }
 }
 </script>
