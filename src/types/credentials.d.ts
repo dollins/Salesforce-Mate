77599c22730c8afc8d5f3ac0de5e721f12ba1e46
Petr Bartoníček <bartonicekpetr@gmail.com>
2023-05-05 23:50:21 +0200

    Credentials definition.

--- src/types/credentials.d.ts
+++ src/types/credentials.d.ts
@@ -1,6 +1,21 @@
 export * from './credentials';
 
+type OrgType = 'Prod' | 'Test';
 export interface Credentials {
-    username: string;
-    password: string;
+    id: String;
+    username: String;
+    password: String;
+    randomSalt: String;
+    clientId: String;
+    clientSecret: String;
+    secretToken: String;
+    sessionId: String;
+    refreshToken: String;
+    jwt: String;
+    orgType: OrgType;
+    orgName: String;
+    apiVersionActive: Number;
+    apiDefaultVersion: Number;
+    createdDate: Date;
+    lastModifiedDate: Date;
 }
