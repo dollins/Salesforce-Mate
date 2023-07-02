c8e0882dbf9d7ae6fee56dd1924af1b867ab00fd
dollins <bartonicekpetr@gmail.com>
2023-07-02 20:02:57 +0200

    Tsconfig and ts fix. Use es6.

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import PopupCmp from './popupCmp.jsx'
+import PopupCmp from './popupCmp'
 
 const root = createRoot(document.getElementById('app'));
 root.render(<PopupCmp />);
